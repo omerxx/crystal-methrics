2efc11ea475c7f7aa8c4e4225ececce413da5ea0
omerxx <omerxx@gmail.com>
2018-04-27 18:17:50 +0300

    First commit

---
+++ src/crystal-methrics.cr
@@ -0,0 +1,6 @@
+require "./crystal-methrics/*"
+
+# TODO: Write documentation for `Crystal::Methrics`
+module Crystal::Methrics
+  # TODO: Put your code here
+end
---
+++ src/crystal-methrics/version.cr
@@ -0,0 +1,3 @@
+module Crystal::Methrics
+  VERSION = "0.1.0"
+end
