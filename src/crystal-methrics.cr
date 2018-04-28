6e98b3853498fc4d3359cf13d8d1bd42293e2c34
omerxx <omerxx@gmail.com>
2018-04-28 19:59:52 +0300

    WIP with redis

--- src/crystal-methrics.cr
+++ src/crystal-methrics.cr
@@ -1,23 +1,4 @@
-require "./crystal-methrics/*"
-require "router"
+#require "./crystal-methrics/router/run"
 
-# TODO: Write documentation for `Crystal::Methrics`
-module Crystal::Methrics
-  class WebServer
-    include Router
+require "./crystal-methrics/redis/redis"
 
-    def draw_routes
-      get "/" do |context, params|
-        context.response.print "Hello WORLD"
-        context
-      end
-    end
-
-    def run
-      port = ENV["PORT"] ||= "3000"
-      server = HTTP::Server.new(port.to_i, route_handler)
-      puts "Listening on #{port}..."
-      server.listen
-    end
-  end
-end
